b153f89e12f3bfc2ba0dded8aef038e43d578a7a
byhsieh <tcfsh24001015@gamil.com>
2020-03-25 03:12:57 +0800

    Add 4_2_textLCD/

--- 4_2_textLCD/main.cpp
+++ 4_2_textLCD/main.cpp
@@ -11,24 +11,21 @@ TextLCD lcd(D2, D3, D4, D5, D6, D7);
 int main()
 
 {
+      int x = 30;
+      lcd.printf("ID_NUM:105070039\n");
 
-      int x=0;
-
-      lcd.printf("Hello World!\n");
-
-      while(true)
+      while(x >= 0)
 
       {
-
             led = !led;             // toggle led
 
-            lcd.locate(5,1);
+            lcd.locate(7,1);
 
-            lcd.printf("%5i",x);    //conuter display
+            lcd.printf("%2D",x);    //conuter display
 
             wait(1);
 
-            x++;
+            --x;
 
       }
 
